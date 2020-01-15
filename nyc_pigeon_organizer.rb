b1210caabe1fd50e86fbaa179b389921458988cb
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-15 20:58:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+final={}  
+
+data.each do |color_gender_lives,all_other|
+  all_other.each do |category, array|
+    array.each do |name|
+      final[name]={:color => [],:gender =>[], :lives =>[]}
+    end
+  end
 end
+
+x =final.keys
+
+data[:color].each do |bird_color,name|
+  name.each do |bird_name|
+    x.each do |item|
+      if bird_name === item
+        final[item][:color] << bird_color.to_s
+    end
+  end
+end
+end
+
+data[:gender].each do |gender,name|
+  name.each do |bird_name|
+    x.each do |item|
+       if bird_name === item
+         final[item][:gender].push(gender.to_s)
+     end
+    end
+  end
+end
+
+
+
+return final
+end 
+
+
